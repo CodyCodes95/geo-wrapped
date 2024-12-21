e8e83de4904bc9d65229dd283f110f6e4215c561
Cody Partington <codythatsme@gmail.com>
2024-12-21 17:56:40 +1100

    url copy fix

--- src/app/_components/UploadDropzone.tsx
+++ src/app/_components/UploadDropzone.tsx
@@ -1,6 +1,8 @@
+"use client";
 import { CheckIcon, CopyIcon } from "lucide-react";
 import React, { useState } from "react";
 import { toast } from "sonner";
+import { Card, CardContent, CardTitle } from "~/components/ui/card";
 import { UploadDropzone } from "~/utils/uploadthing";
 
 const Upload = () => {
@@ -15,33 +17,33 @@ const Upload = () => {
 
   if (wrappedUrl) {
     return (
-      <div className="rounded-xl p-6 shadow-lg">
-        <h3 className="mb-4 text-lg font-medium">
-          Your results are being processed!
-        </h3>
-        <div className="flex items-center gap-2 rounded-lg p-3">
-          <input
-            type="text"
-            value={wrappedUrl}
-            readOnly
-            className="flex-1 bg-transparent outline-none"
-          />
-          <button
-            onClick={copyToClipboard}
-            className="rounded-md p-2 transition-colors hover:bg-primary-foreground"
-            title="Copy URL"
-          >
-            {copied ? (
-              <CheckIcon className="h-5 w-5 text-primary" />
-            ) : (
-              <CopyIcon className="h-5 w-5 text-secondary" />
-            )}
-          </button>
-        </div>
-        <p className="mt-4 text-sm">
-          Come back in about 30 minutes to see your year in review!
-        </p>
-      </div>
+      <Card className="flex flex-col gap-4 rounded-xl p-6 shadow-lg">
+        <CardTitle>Your results are being processed!</CardTitle>
+        <CardContent>
+          <div className="flex items-center gap-2 rounded-lg bg-primary-foreground p-3">
+            <input
+              type="text"
+              value={wrappedUrl}
+              readOnly
+              className="flex-1 bg-transparent outline-none"
+            />
+            <button
+              onClick={copyToClipboard}
+              className="rounded-md p-2 transition-colors hover:bg-primary-foreground"
+              title="Copy URL"
+            >
+              {copied ? (
+                <CheckIcon className="h-5 w-5 text-primary" />
+              ) : (
+                <CopyIcon className="h-5 w-5 text-white" />
+              )}
+            </button>
+          </div>
+          <p className="mt-4 text-sm">
+            Come back in about 30 minutes to see your year in review!
+          </p>
+        </CardContent>
+      </Card>
     );
   }
 
@@ -51,7 +53,7 @@ const Upload = () => {
       endpoint="geoDataUploader"
       onClientUploadComplete={(res) => {
         // Do something with the response
-        setWrappedUrl(res[0]!.serverData.url);
+        setWrappedUrl(`${window.location.origin}/${res[0]!.serverData.url}`);
       }}
       onUploadError={(error: Error) => {
         toast.error(
